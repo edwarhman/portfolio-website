64f5b5c4091049e09f601af27350a77e84ecbac6
edwarhman <edwarhman@gmail.com>
2021-11-14 10:47:15 -0400

    add: project-description animation when scroll

--- scripts.js
+++ scripts.js
@@ -15,3 +15,37 @@ document.getElementById("nav-bar").addEventListener('click', e => {
 document.querySelector('h1').addEventListener('click', e => {
   console.log(document.getElementById('projectsSection').offsetTop);
 })
+
+
+function isElementInRange(el, top, bottom, left, right) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= top &&
+    rect.bottom <= bottom &&
+    (left === undefined || rect.left >= left) &&
+    (right === undefined || rect.right <= right)
+  )
+}
+
+let projectsCards = Array.from(document.querySelectorAll('.project-card'));
+
+const mouseover = new MouseEvent('mouseover', {
+  'view': window,
+  'bubbles': true,
+  'cancelable': false
+});
+
+document.addEventListener('scroll', ()=> {
+  if (document.documentElement.clientWidth < 460) {
+    const rect = projectsCards[0].getBoundingClientRect();
+    projectsCards.forEach((card) => {
+      if (isElementInRange(card, 0, rect.height * 2.5)) {
+        console.log(`card ${card} is in range`);
+        card.lastElementChild.firstElementChild.classList.add('showed');
+      } else {
+        card.lastElementChild.firstElementChild.classList.remove('showed');
+      }
+
+    });
+  }
+});
