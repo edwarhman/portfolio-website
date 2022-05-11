750e7f1ab15cc529bc2dbf3e651d5a30203ce1cd
edwarhman <edwarhman@gmail.com>
2022-05-11 12:47:18 -0400

    fix: add linkedin profile link

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
-const navLinks = [].slice.call(document.querySelectorAll('.nav-link'));
-const projectsCards = Array.from(document.querySelectorAll('.project-card'));
-const navBar = document.getElementById('nav-bar');
-const sections = Array.from(document.querySelectorAll('.main-section'));
+const navLinks = [].slice.call(document.querySelectorAll(".nav-link"));
+const projectsCards = Array.from(document.querySelectorAll(".project-card"));
+const navBar = document.getElementById("nav-bar");
+const sections = Array.from(document.querySelectorAll(".main-section"));
 
 function isElementInRange(el, strict, top, bottom, left, right) {
   const rect = el.getBoundingClientRect();
@@ -11,7 +11,7 @@ function isElementInRange(el, strict, top, bottom, left, right) {
       rect.bottom <= bottom &&
       (left === undefined || rect.left >= left) &&
       (right === undefined || rect.right <= right)
-    )
+    );
   }
 
   return (
@@ -21,38 +21,44 @@ function isElementInRange(el, strict, top, bottom, left, right) {
     (left >= rect.bottom && right <= rect.right) ||
     (rect.left >= left && rect.left <= right) ||
     (rect.right >= left && rect.right <= right)
-  )
+  );
 }
 
-document.addEventListener('click', e => {
+document.addEventListener("click", (e) => {
   const navLink = e.target.parentElement;
-  if ( navLinks.indexOf(navLink) >= 0) {
-    navLinks.forEach(el => {
-      el.classList.remove('active');
+  if (navLinks.indexOf(navLink) >= 0) {
+    navLinks.forEach((el) => {
+      el.classList.remove("active");
     });
-    console.log(e.target.parentElement);  
+    console.log(e.target.parentElement);
     e.target.parentElement.classList.add("active");
   }
-})
+});
 
-document.addEventListener('scroll', ()=> {
+document.addEventListener("scroll", () => {
   if (document.documentElement.clientWidth < 460) {
     const cardRect = projectsCards[0].getBoundingClientRect();
     projectsCards.forEach((card) => {
       if (isElementInRange(card, true, 0, cardRect.height * 2.5)) {
         console.log(`card ${card} is in range`);
-        card.lastElementChild.firstElementChild.classList.add('showed');
+        card.lastElementChild.firstElementChild.classList.add("showed");
       } else {
-        card.lastElementChild.firstElementChild.classList.remove('showed');
+        card.lastElementChild.firstElementChild.classList.remove("showed");
       }
-
     });
   }
 
   sections.forEach((section, index) => {
-    if(isElementInRange(section, false, window.innerHeight * 0.3, window.innerHeight * 0.5)) {
-      navLinks.forEach(link => link.classList.remove('active'));
-      navLinks[index].classList.add('active');
+    if (
+      isElementInRange(
+        section,
+        false,
+        window.innerHeight * 0.3,
+        window.innerHeight * 0.5
+      )
+    ) {
+      navLinks.forEach((link) => link.classList.remove("active"));
+      navLinks[index].classList.add("active");
     }
   });
 });
