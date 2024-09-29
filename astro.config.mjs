a90588e17eeb16965f5c994737eec25445fbd0af
Emerson Warhman <edwarhman@gmail.com>
2024-09-29 19:27:27 -0400

    move noise.svg to src folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import vercel from '@astrojs/vercel/serverless';
-
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: vercel({
-    imageService: true,
-  })
 });
